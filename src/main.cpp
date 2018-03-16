7fd7dda0b63e0ac0e9272f57f9ce5f8dbb85adb3
b45i <amalshajan2011@gmail.com>
2018-03-16 23:45:36 +0530

    basic text to braille algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,77 @@
 #include <Arduino.h>
 
+int braillePins[] = {D1, D2, D3, D4, D5, D6};
+
+void testPins() {
+	// For testing connections
+	for(int i=0;i<6;i++) {
+		digitalWrite(braillePins[i], HIGH);
+		delay(1000);
+	}
+
+	for(int i=0;i<6;i++) {
+		digitalWrite(braillePins[i], LOW);
+		delay(1000);
+	}
+}
+
+void displayLetter(int letterValue[6]) {
+	for(int i=0;i<6;i++) {
+		digitalWrite(braillePins[i], letterValue[i]);
+		delay(1000);
+	}
+}
+
+void displayString(String newsString) {
+
+	int alphabets[][6] = {
+		{ 1, 0, 0, 0, 0, 0 },
+		{ 1, 1, 0, 0, 0, 0 },
+		{ 1, 0, 0, 1, 0, 0 },
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		// {,,,,,},
+		{ 1, 0, 1, 0, 1, 1}
+	};
+
+	for(int i=0;i<newsString.length();i++) {
+		if(islower(newsString[i])) {
+			displayLetter(alphabets[(int)newsString[i]-97]);
+		}
+	}
+}
+
+void setupPins() {
+	for(int i=0;i<6;i++) {
+		pinMode(braillePins[i], OUTPUT);
+	}
+}
+
 void setup() {
-    // put your setup code here, to run once:
+	Serial.begin(115200);
+	setupPins();
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+	displayString(Serial.readString());
 }
